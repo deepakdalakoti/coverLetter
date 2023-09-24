2ef489516ee16f430b817c7a0b97f74e06236e7d
Deepak <deepakkdalakoti@gmail.com>
2023-09-24 11:15:00 +1000

    temp files

--- src/streamlit.py
+++ src/streamlit.py
@@ -2,16 +2,20 @@ from llm import LLM
 from parsers import Parser
 import streamlit as st
 import time
+import tempfile
+from datetime import datetime
 
 
 def generate():
     if file is None or jd is None:
         return
+
     with st.spinner("Generating..."):
-        with open("cv.pdf", "wb") as f:
-            f.write(file.getvalue())
+        temp_file = tempfile.NamedTemporaryFile()
+        temp_file.write(file.getvalue())
         doc_parser = Parser()
-        text = doc_parser.parse("cv.pdf", "cv.pdf")
+        text = doc_parser.parse(file.name, temp_file.name)
+        temp_file.close()
         output = LLM().generate_output(text, jd, **{"temperature": add_slider})
 
     st.session_state["generated_text"] = output
@@ -28,7 +32,7 @@ file = st.sidebar.file_uploader("Upload CV (pdf/word)", type=["pdf", "docx"])
 jd = st.sidebar.text_area(label="Add job description")
 
 # Add a slider to the sidebar:
-add_slider = st.sidebar.slider("Creativity", 0.0, 2.0, 0.5)
+add_slider = st.sidebar.slider("Creativity", 0.0, 1.0, 0.2)
 action_button = st.sidebar.button("Generate", on_click=generate)
 
 placeholder = st.empty()
